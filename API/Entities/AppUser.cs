bfbe5b0d95c341595e2d9fa7e82d7acb01dbf18a
jon-cron <jonathanacm95@gmail.com>
2023-02-28 08:47:57 -0700

    Exclude note in AppUser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,5 @@
 namespace API.Entities;
-
+// NOTE if you would like you can hide files from your view in the settings by adding a pattern to "Exclude" **/.evn for example
 public class AppUser
 {
     public int Id { get; set; } 
